51520813da774298c31d290cbdaeb32ed7d7c9be
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-07-23 06:59:17 +0200

    added smart transports

--- src/transport.h
+++ src/transport.h
@@ -4,15 +4,20 @@
 #include <lauxlib.h>
 
 #define LGIT_TRANSPORT_FUNCS "is.westh.lgit.transport"
+#define checktransport(L, N) \
+      (git_transport**) luaL_checkudata( L, N, LGIT_TRANSPORT_FUNCS )
+#define checktransport(L) checktransport( L, 1 )
 
 int lgit_transport_new( lua_State *L );
 int lgit_transport_local( lua_State *L );
 int lgit_transport_dummy( lua_State *L );
+int lgit_transport_smart( lua_State *L );
 
 static const struct luaL_Reg lgit_transport_funcs [] = {
-   { "from_url", lgit_transport_new },
+   { "from_url",     lgit_transport_new   },
    { "create_local", lgit_transport_local },
    { "create_dummy", lgit_transport_dummy },
+   { "create_smart", lgit_transport_smart },
    { NULL, NULL }
 };
 #endif// IS_LGIT_TRANSPORT
