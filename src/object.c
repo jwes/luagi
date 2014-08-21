1b8c2c362d9a0abed6465a0494afaa619135d398
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-08-21 18:25:28 +0200

    implemneted object lookup functions

--- src/object.c
+++ src/object.c
@@ -1,36 +1,97 @@
 #include "object.h"
 #include <git2/object.h>
+#include <git2/oid.h>
 #include "wien.h"
-
 int luagi_object_lookup( lua_State *L )
 {
-   luaL_error( L, "not implemented" );
-   return 0;
+   git_repository **repo = checkrepo( L, 1 );
+   const char *idstr = luaL_checkstring( L, 2 );
+   const char *typestr = luaL_checkstring( L, 3 );
+
+   git_oid id;
+   if( git_oid_fromstr( &id, idstr ) )
+   {
+      ERROR_PUSH( L )
+   }
+
+   git_otype type = git_object_string2type( typestr );
+  
+   git_object **out = lua_newuserdata( L, sizeof( git_object *) );
+   if( git_object_lookup( out, *repo, &id, type ) )
+   {
+      ERROR_PUSH( L )
+   }
+
+   luaL_getmetatable(L, LGIT_OBJECT_FUNCS );
+   lua_setmetatable(L, -2);
+
+   return 1;
 }
+
 int luagi_object_lookup_bypath( lua_State *L )
 {
-   luaL_error( L, "not implemented" );
-   return 0;
+   git_object **obj = checkobject_at( L, 1 );
+   const char *path = luaL_checkstring( L, 2 );
+   const char *typestr = luaL_checkstring( L, 3 );
+
+   git_otype type = git_object_string2type( typestr );
+  
+   git_object **out = lua_newuserdata( L, sizeof( git_object *) );
+   if( git_object_lookup_bypath( out, *obj, path, type ) )
+   {
+      ERROR_PUSH( L )
+   }
+
+   luaL_getmetatable(L, LGIT_OBJECT_FUNCS );
+   lua_setmetatable(L, -2);
+
+   return 1;
 }
 
 //use
 int luagi_object_id( lua_State *L )
 {
-   luaL_error( L, "not implemented" );
-   return 0;
+   git_object **obj = checkobject_at( L, 1 );
+
+   const git_oid *oid = git_object_id( *obj );
+   if( oid == NULL )
+   {
+      ERROR_PUSH( L )
+   }
+
+   char buf[ GIT_OID_HEXSZ + 1 ];
+   lua_pushstring( L, git_oid_tostr( buf, sizeof( buf ), oid ) );
+   return 1;
 }
-int luagi_object_short_id( lua_State *L )
-{
-   luaL_error( L, "not implemented" );
-   return 0;
-}
+
 int luagi_object_type( lua_State *L )
 {
-   luaL_error( L, "not implemented" );
-   return 0;
+   git_object **obj = checkobject_at( L, 1 );
+
+   git_otype type = git_object_type( *obj );
+   lua_pushstring( L, git_object_type2string( type ) );
+
+   return 1;
 }
+
 int luagi_object_owner( lua_State *L )
 {
-   luaL_error( L, "not implemented" );
-   return 0;
+   git_object **obj = checkobject_at( L, 1 );
+   git_repository **repo = lua_newuserdata( L, sizeof( git_repository *) );
+   *repo = git_object_owner( *obj );
+   if( *repo == NULL )
+   {
+      ERROR_PUSH( L )
+   }
+   luaL_getmetatable(L, REPO_NAME );
+   lua_setmetatable(L, -2);
+   return 1;
+}
+
+int luagi_object_free( lua_State *L )
+{
+
+   git_object **obj = checkobject_at( L, 1 );
+   git_object_free( *obj );
+   return 0;   
 }
--- src/object.h
+++ src/object.h
@@ -9,7 +9,6 @@ int luagi_object_lookup_bypath( lua_State *L );
 
 //use
 int luagi_object_id( lua_State *L );
-int luagi_object_short_id( lua_State *L );
 int luagi_object_type( lua_State *L );
 int luagi_object_owner( lua_State *L );
 int luagi_object_free( lua_State *L );
@@ -28,7 +27,6 @@ int luagi_object_peel( lua_State *L );
 
 static const struct luaL_Reg lgit_index_funcs [] = {
    { "id",   luagi_object_id },
-   { "short_id",   luagi_object_short_id },
    { "type",   luagi_object_type },
    { "owner",   luagi_object_owner },
    { "__gc",   luagi_object_free },
