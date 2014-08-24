2fc48afd82a6f77b18f4ead7dcf4a03b2a02cf80
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-08-24 10:29:58 +0200

    stash foreach with function callback

--- src/stash.c
+++ src/stash.c
@@ -31,10 +31,46 @@ int luagi_stash_save( lua_State *L )
    lua_pushstring( L, git_oid_tostr( buf, sizeof( buf ), &out ) );
    return 1;
 }
+struct foreach_payload
+{
+   lua_State *L;
+   int callback_pos;
+};
+
+static int callback ( size_t index, const char *message, 
+                const git_oid *stash_id, void *payload )
+{
+   struct foreach_payload *p = payload;
+
+   lua_pushvalue( p->L, p->callback_pos );
+   lua_pushinteger( p->L, index );
+   lua_pushstring( p->L, message );
+
+   char buf[ GIT_OID_HEXSZ + 1];
+   lua_pushstring( p->L, git_oid_tostr( buf, sizeof( buf ), stash_id ) );
+   if( lua_pcall( p->L, 3, 1, 0 ) != LUA_OK )
+   {
+      luaL_error( p->L, "can not call stash callback" );
+   }
+
+   int ret = luaL_checkinteger( p->L, -1 );
+   lua_pop( p->L, 1 );
+   return ret;
+}
 
 int luagi_stash_foreach( lua_State *L )
 {
-   luaL_error( L, "not yet implemented" );
+   git_repository **repo = checkrepo( L, 1 );
+   struct foreach_payload *p = malloc( sizeof( struct foreach_payload ) );
+   luaL_checktype( L, 2, LUA_TFUNCTION );
+   p->L = L;
+   p->callback_pos = 2;
+
+   if( git_stash_foreach( *repo, callback, p ) )
+   {
+      ERROR_ABORT( L )
+   }
+   free( p );
    return 0;
 }
 
