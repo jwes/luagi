339378d10ed0c0de879fa7917af2b23d69baa091
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-08-25 07:27:46 +0200

    first creator functions for references

--- src/reference.c
+++ src/reference.c
@@ -1,9 +1,55 @@
 #include "reference.h"
 #include <git2/refs.h>
+#include <git2/oid.h>
+#include "luagi.h"
 
-int luagi_reference_lookup( lua_State *L ){ luaL_error( L, "not yet implemented"); return 0; }
-int luagi_reference_name_to_id( lua_State *L ){ luaL_error( L, "not yet implemented"); return 0; }
-int luagi_reference_dwim( lua_State *L ){ luaL_error( L, "not yet implemented"); return 0; }
+int luagi_reference_lookup( lua_State *L )
+{
+   git_repository **repo = checkrepo( L, 1 );
+   const char *name = luaL_checkstring( L, 2 );
+
+   git_reference **out = lua_newuserdata( L, sizeof( git_reference *) );
+   if( git_reference_lookup( out, *repo, name ) )
+   {
+      ERROR_PUSH( L )
+   }
+
+   luaL_getmetatable( L, LUAGI_REFERENCE_FUNCS );
+   lua_setmetatable( L, -2 );
+   return 1;
+}
+
+int luagi_reference_name_to_id( lua_State *L )
+{
+   git_repository **repo = checkrepo( L, 1 );
+   const char *name = luaL_checkstring( L, 2 );
+
+   git_oid out;
+   if(git_reference_name_to_id( &out, *repo, name ) )
+   {
+      ERROR_PUSH( L )
+   }
+
+   char buf[GIT_OID_HEXSZ + 1 ];
+   lua_pushstring( L, git_oid_tostr( buf, sizeof(buf), &out ) );
+   return 1;
+}
+   
+int luagi_reference_dwim( lua_State *L )
+{
+   git_repository **repo = checkrepo( L, 1 );
+   const char *shorthand = luaL_checkstring( L, 2 );
+
+   git_reference **out = lua_newuserdata( L, sizeof( git_reference * ) );
+
+   if( git_reference_dwim( out, *repo, shorthand ) )
+   {
+      ERROR_PUSH( L )
+   }
+   luaL_getmetatable( L, LUAGI_REFERENCE_FUNCS );
+   lua_setmetatable( L, -2 );
+   return 1;
+}
 
 int luagi_reference_symbolic_create_matching( lua_State *L ){ luaL_error( L, "not yet implemented"); return 0; }
 int luagi_reference_symbolic_create( lua_State *L ){ luaL_error( L, "not yet implemented"); return 0; }
