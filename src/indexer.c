a134cf1391c0b180713b63906b287263497bc0dc
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-09-23 06:45:46 +0200

    indexer functions

--- src/indexer.c
+++ src/indexer.c
@@ -5,8 +5,61 @@
 #include "odb.h"
 #include "oid.h"
 
-int luagi_indexer_new( lua_State *L );
-int luagi_indexer_append( lua_State *L );
+static int luagi_transfer_progress_cb( const git_transfer_progress *stats, void *payload )
+{
+   luagi_foreach_t *p = payload;
+
+   lua_pushvalue( p->L, p->callback_pos );
+   luagi_push_transfer_stats( p->L, stats );
+
+   if( lua_pcall( p->L, 1, 1, 0 ) )
+   {
+      luaL_error( p->L, "error calling progress callback" );
+      free( p );
+      return -1;
+   }
+
+   int ret = luaL_checkinteger( p->L, -1 );
+   lua_pop( p->L, 1 );
+   return ret;
+}
+
+int luagi_indexer_new( lua_State *L )
+{
+   git_odb **odb = checkodb_at( L, 1 );
+   const char *path = luaL_checkstring( L, 2 );
+   unsigned int mode = 0; //TODO mode
+ 
+   luaL_checktype( L, 3, LUA_TFUNCTION );
+   luagi_foreach_t *p = malloc( sizeof( luagi_foreach_t ) );
+   p->L = L;
+   p->callback_pos = 3;
+   git_indexer **out = lua_newuserdata( L, sizeof( git_indexer *) );
+
+   if( git_indexer_new(  out, path, mode, *odb, luagi_transfer_progress_cb, p ) )
+   {
+      ERROR_PUSH( L );
+   }
+   luaL_getmetatable( L, LUAGI_INDEXER_FUNCS );
+   lua_setmetatable( L, -2 );
+   return 1;
+}
+
+int luagi_indexer_append( lua_State *L )
+{
+   git_indexer **indexer = checkindexer( L );
+   int size = luaL_len( L, 2 );
+   const char *data = luaL_checkstring( L , 2 );
+
+   git_transfer_progress stats;
+
+   if( git_indexer_append( *indexer, data, size, &stats ) )
+   {
+      ERROR_PUSH( L )
+   }
+   return luagi_push_transfer_stats( L, &stats );
+}
+
 int luagi_indexer_commit( lua_State *L )
 {
    git_indexer **indexer = checkindexer( L );
