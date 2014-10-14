a4c7e59fab2f2f8905040292fb43c7f474ed692e
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-10-14 20:06:15 +0200

    Added Testcase, set config buffer size

--- src/config.c
+++ src/config.c
@@ -6,6 +6,8 @@
 static int luagi_find_config( lua_State *L, int (*func)( git_buf *out ) )
 {
    git_buf buf;
+   buf.size = 0;
+   buf.asize = 0;
    if( func( &buf ) )
    {
       ERROR_PUSH( L )
