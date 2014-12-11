cf0c7aaeeef3b4a7bd41ab21d9c632e0ab6b1c8b
Johannes Westhuis <johannes.westhuis@gmail.com>
2014-12-11 06:56:44 +0100

    further checkout options implemented.
    Callbacks for progress and notify are not supported yet

--- src/checkout.c
+++ src/checkout.c
@@ -4,12 +4,138 @@
 #include "luagi.h"
 #include "index.h"
 #include "object.h"
+#include "tree.h"
 
-int luagi_parse_checkout_options( git_checkout_options *options, lua_State *L, const int tableIndex )
+int luagi_parse_checkout_options( git_checkout_options *opts, lua_State *L, const int tableIndex )
 {
    lua_getfield( L, tableIndex, LUAGI_CHECKOUT_OPTS_VERSION );
    int version = luaL_optinteger( L, -1, GIT_CHECKOUT_OPTIONS_VERSION );
-   git_checkout_init_options( options, version );
+
+   git_checkout_init_options( opts, version );
+   opts->checkout_strategy = GIT_CHECKOUT_NONE;
+
+   lua_getfield( L, tableIndex, FLAGS );
+   if( lua_type( L, -1 ) == LUA_TTABLE )
+   {
+      add_flag( opts->checkout_strategy, L, -1, 
+                      SAFE, GIT_CHECKOUT_SAFE );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      SAFE_CREATE, GIT_CHECKOUT_SAFE_CREATE );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      FORCE, GIT_CHECKOUT_FORCE );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      ALLOW_CONFLICTS, GIT_CHECKOUT_ALLOW_CONFLICTS );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      REMOVE_UNTRACKED, GIT_CHECKOUT_REMOVE_UNTRACKED );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      REMOVE_IGNORED, GIT_CHECKOUT_REMOVE_IGNORED );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      UPDATE_ONLY, GIT_CHECKOUT_UPDATE_ONLY );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      DONT_UPDATE_INDEX, GIT_CHECKOUT_DONT_UPDATE_INDEX );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      NO_REFRESH, GIT_CHECKOUT_NO_REFRESH );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      SKIP_UNMERGED, GIT_CHECKOUT_SKIP_UNMERGED );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      USE_OURS, GIT_CHECKOUT_USE_OURS );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      USE_THEIRS, GIT_CHECKOUT_USE_THEIRS );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      DISABLE_PATHSPEC_MATCH, GIT_CHECKOUT_DISABLE_PATHSPEC_MATCH );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      SKIP_LOCKED_DIRECTORIES, GIT_CHECKOUT_SKIP_LOCKED_DIRECTORIES );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      DONT_OVERWRITE_IGNORED, GIT_CHECKOUT_DONT_OVERWRITE_IGNORED );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      CONFLICT_STYLE_MERGE, GIT_CHECKOUT_CONFLICT_STYLE_MERGE );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      CONFLICT_STYLE_DIFF3, GIT_CHECKOUT_CONFLICT_STYLE_DIFF3 );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      UPDATE_SUBMODULES, GIT_CHECKOUT_UPDATE_SUBMODULES );
+      add_flag( opts->checkout_strategy, L, -1, 
+                      UPDATE_SUBMODULES_IF_CHANGED, GIT_CHECKOUT_UPDATE_SUBMODULES_IF_CHANGED );
+   }
+
+   lua_getfield( L, tableIndex, DISABLE_FILTERS );
+   opts->disable_filters = lua_toboolean( L, -1 );
+
+   lua_getfield( L, tableIndex, DIR_MODE );
+   unsigned int dir_mode = luaL_optinteger(L, -1, -1 );
+   if( dir_mode < 0xFFFFu )
+   {
+      opts->dir_mode = dir_mode;
+   }
+
+   lua_getfield( L, tableIndex, FILE_MODE );
+   unsigned int file_mode = luaL_optinteger(L, -1, -1 );
+   if( file_mode < 0xFFFFu )
+   {
+      opts->file_mode = file_mode;
+   }
+
+   lua_getfield( L, tableIndex, FILE_OPEN_FLAGS );
+   int file_open_flags = luaL_optinteger(L, -1, -1 );
+   if( file_open_flags >= 0 )
+   {
+      opts->file_open_flags = file_open_flags;
+   }
+
+   lua_getfield( L, tableIndex, NOTIFY_FLAGS );
+   if( lua_type( L, -1 ) == LUA_TTABLE )
+   {
+      add_flag( opts->notify_flags, L, -1, 
+                      NONE, GIT_CHECKOUT_NOTIFY_NONE );
+      add_flag( opts->notify_flags, L, -1, 
+                      CONFLICT, GIT_CHECKOUT_NOTIFY_CONFLICT );
+      add_flag( opts->notify_flags, L, -1, 
+                      DIRTY, GIT_CHECKOUT_NOTIFY_DIRTY );
+      add_flag( opts->notify_flags, L, -1, 
+                      UPDATED, GIT_CHECKOUT_NOTIFY_UPDATED );
+      add_flag( opts->notify_flags, L, -1, 
+                      UNTRACKED, GIT_CHECKOUT_NOTIFY_UNTRACKED );
+      add_flag( opts->notify_flags, L, -1, 
+                      IGNORED, GIT_CHECKOUT_NOTIFY_IGNORED );
+      add_flag( opts->notify_flags, L, -1, 
+                      ALL, GIT_CHECKOUT_NOTIFY_ALL );
+   }
+   // TODO: NOTIFY AND PROGRESS Callbacks 
+   
+   lua_getfield( L, tableIndex, PATHS );
+   if( lua_type( L, -1 ) == LUA_TTABLE )
+   {
+      opts->paths = luagi_strings_from_lua_list( L, -1 );
+   }
+
+   lua_getfield( L, tableIndex, BASELINE );
+   if( lua_type( L, -1 ) == LUA_TUSERDATA )
+   {
+      opts->baseline = *(git_tree **)luaL_checkudata( L, -1, LUAGI_TREE_FUNCS );
+   }
+
+   lua_getfield( L, tableIndex, TARGET );
+   if( lua_type( L, -1 ) == LUA_TSTRING )
+   {
+      opts->target_directory = luaL_checkstring( L, -1 );
+   }
+
+   lua_getfield( L, tableIndex, ANCESTOR_LABEL );
+   if( lua_type( L, -1 ) == LUA_TSTRING )
+   {
+      opts->ancestor_label = luaL_checkstring( L, -1 );
+   }
+   
+   lua_getfield( L, tableIndex, OUR_LABEL );
+   if( lua_type( L, -1 ) == LUA_TSTRING )
+   {
+      opts->our_label = luaL_checkstring( L, -1 );
+   }
+
+   lua_getfield( L, tableIndex, THEIR_LABEL );
+   if( lua_type( L, -1 ) == LUA_TSTRING )
+   {
+      opts->their_label = luaL_checkstring( L, -1 );
+   }
    return 0;
 }
 
