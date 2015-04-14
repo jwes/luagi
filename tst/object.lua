d36f81d13d98ae9c1c5a80a59b9572ecf23a8d77
Johannes Westhuis <johannes.westhuis@gmail.com>
2015-04-14 19:34:47 +0200

    Object test cases

--- tst/object.lua
+++ tst/object.lua
@@ -1,3 +1,37 @@
-describe( "id #object", function() pending("  luagi_object_id ") end)
-describe( "type #object", function() pending("  luagi_object_type ") end)
-describe( "owner #object", function() pending("  luagi_object_owner ") end)
+local luagi = require( "luagi" )
+local test_helper = require( "test_helper" )
+
+local commit = "3a3e73745d1a2ba679362d51e0a090a3ee03aad6"
+local t = "commit"
+
+describe( "object", function()
+   local repo = nil
+   local obj = nil
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+      if err then return end
+      obj, err = repo:lookup_object( commit, t )
+   end)
+
+   it( "should have no error", function()
+      assert.is.falsy( err )
+   end)
+
+   describe( "id #object", function()
+      it ("should have the right id", function()
+         assert.are.equal( commit, obj:id() )
+      end)
+   end)
+
+   describe( "type #object", function()
+      it( "should be a commit", function()
+         assert.are.equal( t, obj:type() )
+      end)
+   end)
+   describe( "owner #object", function()
+      it( "should be the same repo", function()
+         assert.are.equal( repo:path(), obj:owner():path() )
+      end)
+   end)
+end)
