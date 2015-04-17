7131213af2f526c1ad2afa0b7de2b338d4cbb6f2
Johannes Westhuis <johannes.westhuis@gmail.com>
2015-04-17 19:20:17 +0200

    reset default and index

--- tst/reset.lua
+++ tst/reset.lua
@@ -1,16 +1,20 @@
 local luagi = require( "luagi" )
 local test_helper = require( "test_helper" )
 local parentId = "3a3e73745d1a2ba679362d51e0a090a3ee03aad6"
+local function gen_setup()
+   test_helper.setup()
+   local repo, err = luagi.open( test_helper.path )
+   if err then return repo, err  end
+   local object, err = repo:lookup_object( parentId, "commit" )
+   return repo, err, object
+end 
 
 describe( "reset #reset", function()
    local repo = nil
    local err = nil
    local object = nil
    setup( function()
-      test_helper.setup()
-      repo, err = luagi.open( test_helper.path )
-      if err then return end
-      object, err = repo:lookup_object( parentId, "commit" )
+      repo, err, object = gen_setup()
    end)
 
    it("should be prepared", function()
@@ -28,11 +32,87 @@ describe( "reset #reset", function()
       setup( function()
          repo:reset( object, "soft", test_helper.signature )
       end)
-      it("should be not available anymore", function()
+      it("should not be available anymore", function()
          assert.is.falsy( io.open( test_helper.path.."/.gitignore", "r") )
       end)
    end)
-
 end)
-describe( "reset_default #reset", function() pending("luagi_reset_default") end)
-describe( "reset_index #reset", function() pending("luagi_reset_index") end)
+
+local function prepare_index(path)
+   local repo, err, object = gen_setup()
+   if err then return repo, err, object end
+   local index, err = repo:index()
+   if err then return repo, err, object end
+   file, err = io.open(test_helper.path.."/"..path, "w")
+   if err then return repo, err, object end
+   file:write( "foo bar" )
+   index:add_by_path( path )
+   index:write()
+   return repo, err, object
+end
+
+describe( "reset_default #reset", function()
+   local repo = nil
+   local err = nil
+   local object = nil
+   local path = "testfile"
+   setup( function()
+      repo, err, object = prepare_index(path) 
+   end)
+
+   it("should be prepared", function()
+      assert.is.falsy( err )
+   end)
+
+   it( "should be unmodified", function()
+      local flags = repo:status_file(path)
+      assert.is.True( flags.index_modified )
+   end)
+
+   describe( "reset_default #reset", function()
+      setup( function()
+         path_spec = {}
+         path_spec[1] = path
+         repo:reset_default( object, path_spec )
+      end)
+
+      it("should not be available anymore", function()
+         local flags = repo:status_file(path)
+         assert.is.False( flags.index_modified )
+         assert.is.True( flags.workdir_modified )
+      end)
+   end)
+end)
+
+describe( "reset_index #reset", function()
+   local repo = nil
+   local err = nil
+   local path = "testfile"
+   setup( function()
+      repo, err = prepare_index(path) 
+   end)
+
+   it("should be prepared", function()
+      assert.is.falsy( err )
+   end)
+
+   it( "should be unmodified", function()
+      local flags = repo:status_file(path)
+      assert.is.True( flags.index_modified )
+   end)
+
+   describe( "reset_index #reset", function()
+      setup( function()
+         path_spec = {}
+         path_spec[1] = path
+         repo:reset_index( path_spec )
+      end)
+
+      it("should not be available anymore", function()
+         local flags = repo:status_file(path)
+         assert.is.True( flags.index_deleted )
+         assert.is.True( flags.workdir_new )
+         assert.is.False( flags.workdir_modified )
+      end)
+   end)
+end)
