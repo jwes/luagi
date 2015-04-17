b460e764315144c6021cbe13f767906f086ddf81
Johannes Westhuis <johannes.westhuis@gmail.com>
2015-04-17 20:39:26 +0200

    ignore testcases

--- tst/ignore.lua
+++ tst/ignore.lua
@@ -1,5 +1,72 @@
-describe( "add_ignore_rules #ignore", function() pending("luagi_ignore_add_rules") end)
-describe( "clear_internal_ignore_rules #ignore", function() pending("luagi_ignore_clear_internal_rules") end)
-describe( "is_path_ignored #ignore", function() pending("luagi_ignore_path_is_ignored") end)
+local luagi = require( "luagi" )
+local test_helper = require( "test_helper" )
+
+describe( "add_ignore_rules #ignore", function()
+   local repo = nil
+   local err = nil
+
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+      repo:add_ignore_rules( "some" )
+   end)
+
+   it( "should have no error", function()
+      assert.is.falsy( err )
+   end)
+
+   it( "should ignore some", function()
+      assert.is.True( repo:is_path_ignored( "some" ))
+   end)
 
 
+end)
+
+describe( "clear_internal_ignore_rules #ignore", function()
+   local repo = nil
+   local err = nil
+
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+      repo:add_ignore_rules( "some" )
+   end)
+
+   it( "should have no error", function()
+      assert.is.falsy( err )
+   end)
+
+   it( "should ignore some", function()
+      assert.is.True( repo:is_path_ignored( "some" ))
+   end)
+
+   it( "should ignore some", function()
+      repo:clear_internal_ignore_rules( "some" )
+      assert.is.False( repo:is_path_ignored( "some" ))
+   end)
+end)
+
+describe( "is_path_ignored #ignore", function()
+   local repo = nil
+   local err = nil
+
+   setup( function()
+      test_helper.extract()
+      repo, err = luagi.open( test_helper.path )
+   end)
+
+   it( "should have no error", function()
+      assert.is.falsy( err )
+   end)
+
+   it( "should ignore *.ign", function()
+      assert.is.True( repo:is_path_ignored( "foo.ign" ) )
+   end)
+   it( "should ignore *.ign", function()
+      assert.is.False( repo:is_path_ignored( "foo" ) )
+   end)
+   it( "should ignore some", function()
+      assert.is.False( repo:is_path_ignored( "some" ))
+   end)
+end)
+   
