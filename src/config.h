ce42134e474789e1500e2e9b1dfdca80a8cd2c0a
Johannes Westhuis <johannes.westhuis@gmail.com>
2015-04-20 07:03:52 +0200

    first simple repo testcases

--- src/config.h
+++ src/config.h
@@ -70,7 +70,7 @@ static const struct luaL_Reg luagi_config_funcs [] = {
    { "open_global", luagi_config_open_global },
    { "snapshot", luagi_config_snapshot },
 
-   { "refresk", luagi_config_refresh },
+   { "refresh", luagi_config_refresh },
    { "get_entry", luagi_config_get_entry },
    { "get_int32", luagi_config_get_int32 },
    { "get_int64", luagi_config_get_int64 },
