729d0a58198d72e54b33b4f2f2f5f4d3609c1d03
Johannes Westhuis <johannes.westhuis@gmail.com>
2015-04-24 17:32:00 +0200

    merge tests
    merge files, find merge base merge ref and oid

--- tst/merge.lua
+++ tst/merge.lua
@@ -84,21 +84,132 @@ describe( "merge_base_many #merge", function()
 
 end)
 
-describe( "merge_base_octopus #merge", function() pending("luagi_merge_base_octopus") end)
+describe( "merge_base_octopus #merge", function()
+   local repo = nil
+   local err = nil
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+   end)
 
-describe( "merge_head_from_ref #merge", function() pending("luagi_merge_head_from_ref") end)
-describe( "merge_head_from_fetchhead #merge", function() pending("luagi_merge_head_from_fetchhead") end)
-describe( "merge_head_from_id #merge", function() pending("luagi_merge_head_from_id") end)
+   it( "should find base_ref", function()
+      local input = {}
+      input[1] = head_master
+      input[2] = head_work
+      input[3] = head_master
+      input[4] = head_work
+      input[5] = base_ref
+      local base = nil
+      assert.has_no_error( function()
+         base, err =  repo:merge_base_octopus( input )
+      end)
+      assert.is.falsy( err )
+      assert.are.equal( base_ref, base )
+   end)
+end)
+
+describe( "merge_head_from_ref #merge", function()
+   local repo = nil
+   local err = nil
+   local merge_head = nil
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+      if err then return end
+      local ref,  err = repo:lookup_reference( "refs/heads/work" )
+      if err then return end
+      merge_head, err = repo:merge_head_from_ref( ref )
+   end)
+
+   it( "should be prepared", function()
+      assert.is.falsy( err )
+      assert.is.not_nil( merge_head )
+      assert.are.equal( "userdata", type( merge_head ) )
+   end)
+end)
+
+describe( "merge_head_from_fetchhead #merge", function()
+   pending( "set up test remote")
+end)
+
+describe( "merge_head_from_id #merge", function()
+   local repo = nil
+   local err = nil
+   local merge_head = nil
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+      if err then return end
+
+      merge_head, err = repo:merge_head_from_id( head_work )
+   end)
+
+   it( "should be prepared", function()
+      assert.is.falsy( err )
+      assert.is.not_nil( merge_head )
+      assert.are.equal( "userdata", type( merge_head ) )
+      assert.is.not_nil( merge_head.__tostring )
+   end)
+end)
+
 describe( "merge_trees #merge", function() pending("luagi_merge_trees") end)
 describe( "merge_commits #merge", function() pending("luagi_merge_commits") end)
 describe( "merge #merge", function() pending("luagi_merge") end)
 describe( "merge_file_from_index #merge", function() pending("luagi_merge_file_from_index") end)
 describe( "merge_analysis #merge", function() pending("luagi_merge_analysis") end)
 
-describe( "__tostring #merge", function() pending("luagi_merge_head_id ") end)
-describe( "automergable #merge", function() pending("luagi_mergeresult_automergable ") end)
-describe( "path #merge", function() pending("luagi_mergeresult_path ") end)
-describe( "mode #merge", function() pending("luagi_mergeresult_mode ") end)
-describe( "content #merge", function() pending("luagi_mergeresult_content ") end)
+describe( "merge_files #merge #base", function()
+   local repo = nil
+   local err = nil
+   local result = nil
+   local path = test_helper.path.."/testfile"
+   setup( function()
+      test_helper.setup()
+      repo, err = luagi.open( test_helper.path )
+      if err then return end
 
-describe( "merge_files #merge #base", function() pending("pending") end)
+      local file = io.open( path , "r" )
+      local content = file:read("*all")
+
+      local ancestor = {}
+      ancestor[ "path" ] = path
+      ancestor[ "content" ] = content
+      local ours  = {}
+      ours[ "path" ] = path
+      ours[ "content" ] = content.."ours"
+      local theirs  = {}
+      theirs[ "path" ] = path
+      theirs[ "content" ] = content.."theirs"
+      result, err = luagi.merge_files( ancestor, ours, theirs )
+   end)
+
+   it( "should be prepared", function()
+      assert.is.falsy( err )
+      assert.is.not_nil( result )
+      assert.are.equal( "userdata", type( result ) )
+      assert.is.not_nil( result.automergable )
+   end)
+
+   describe( "automergable #merge", function()
+      it("should produce conflicts", function()
+         assert.is.False( result:automergable() )
+      end)
+   end)
+
+   describe( "path #merge", function()
+      it( "should return path", function()
+         assert.are.equal( path, result:path() )
+      end)
+   end)
+
+   describe( "mode #merge", function()
+      it( "should return the filemode", function()
+         assert.are.equal( 33188, result:mode() )
+      end)
+   end)
+   describe( "content #merge", function()
+      it( "should return a mergable content", function()
+         assert.is.truthy( result:content():find( "<<<<<" ) )
+      end)
+   end)
+end)
